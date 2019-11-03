eb1cbfd046a3628b92c56255a74b77673ac32928
cecotodorov <cvetomir.todorov@gmail.com>
2019-11-03 20:01:27 +0200

    Extract reusable fixture logic into a base fixture class

---
+++ src/PolyMessage.IntegrationTests/BaseFixture.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using PolyMessage.Binary;
+using PolyMessage.IntegrationTests.RequestResponse;
+using PolyMessage.Tcp;
+using Xunit.Abstractions;
+
+namespace PolyMessage.IntegrationTests
+{
+    public abstract class BaseFixture : IDisposable
+    {
+        protected IServiceProvider ServiceProvider { get; }
+        protected ILogger Logger { get; }
+        protected Uri ServerAddress { get; }
+        protected PolyHost Host { get; }
+        protected List<PolyClient> Clients { get; }
+
+        protected BaseFixture(ITestOutputHelper output)
+        {
+            ServiceProvider = CreateServiceProvider(output);
+            Logger = ServiceProvider.GetRequiredService<ILoggerFactory>().CreateLogger(GetType());
+
+            ServerAddress = GetServerAddress();
+            Host = CreateHost(ServerAddress, ServiceProvider);
+            Clients = new List<PolyClient>();
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposingInsteadOfFinalizing: true);
+        }
+
+        protected virtual void Dispose(bool disposingInsteadOfFinalizing)
+        {
+            if (disposingInsteadOfFinalizing)
+            {
+                foreach (PolyClient client in Clients)
+                {
+                    client.Dispose();
+                }
+                Host.Dispose();
+            }
+        }
+
+        private static IServiceProvider CreateServiceProvider(ITestOutputHelper output)
+        {
+            IServiceCollection services = new ServiceCollection()
+                .AddLogging(loggingBuilder =>
+                {
+                    loggingBuilder.SetMinimumLevel(LogLevel.Information);
+                    loggingBuilder.AddDebug();
+                    loggingBuilder.AddProvider(new XunitLoggingProvider(output));
+                });
+            services.AddScoped<IStringContract, StringImplementor>();
+
+            IServiceProvider serviceProvider = services.BuildServiceProvider();
+            return serviceProvider;
+        }
+
+        private static Uri GetServerAddress()
+        {
+            string hostName = Dns.GetHostName();
+            IPAddress[] addresses = Dns.GetHostAddresses(hostName);
+            IPAddress ipv4Address = addresses.First(a => a.AddressFamily == AddressFamily.InterNetwork);
+
+            UriBuilder addressBuilder = new UriBuilder("tcp", ipv4Address.ToString(), 10678);
+            return addressBuilder.Uri;
+        }
+
+        private static PolyHost CreateHost(Uri serverAddress, IServiceProvider serviceProvider)
+        {
+            ITransport hostTransport = new TcpTransport(serverAddress);
+            IFormat hostFormat = new BinaryFormat();
+            PolyHost host = new PolyHost(hostTransport, hostFormat, serviceProvider);
+            return host;
+        }
+
+        protected static PolyClient CreateClient(Uri serverAddress, IServiceProvider serviceProvider)
+        {
+            ITransport clientTransport = new TcpTransport(serverAddress);
+            IFormat clientFormat = new BinaryFormat();
+            PolyClient client = new PolyClient(clientTransport, clientFormat, serviceProvider.GetRequiredService<ILoggerFactory>());
+            return client;
+        }
+
+        protected async Task StartHost()
+        {
+            Host.Start();
+            await Task.Delay(100);
+        }
+    }
+}
--- src/PolyMessage.IntegrationTests/RequestResponse/Tests.cs
+++ src/PolyMessage.IntegrationTests/RequestResponse/Tests.cs
@@ -2,93 +2,19 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Net;
-using System.Net.Sockets;
 using System.Threading.Tasks;
 using FluentAssertions;
 using FluentAssertions.Execution;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using PolyMessage.Binary;
-using PolyMessage.Tcp;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace PolyMessage.IntegrationTests.RequestResponse
 {
-    public class Tests : IDisposable
+    public class Tests : BaseFixture
     {
-        private readonly IServiceProvider _serviceProvider;
-        private readonly ILogger _logger;
-        private readonly Uri _serverAddress;
-        private readonly PolyHost _host;
-        private readonly List<PolyClient> _clients;
-
-        public Tests(ITestOutputHelper output)
-        {
-            _serviceProvider = CreateServiceProvider(output);
-            _logger = _serviceProvider.GetRequiredService<ILoggerFactory>().CreateLogger(GetType());
-
-            _serverAddress = GetServerAddress();
-            _host = CreateHost(_serverAddress, _serviceProvider);
-            _clients = new List<PolyClient>();
-        }
-
-        public void Dispose()
-        {
-            foreach (PolyClient client in _clients)
-            {
-                client.Dispose();
-            }
-            _host.Dispose();
-        }
-
-        private IServiceProvider CreateServiceProvider(ITestOutputHelper output)
-        {
-            IServiceCollection services = new ServiceCollection()
-                .AddLogging(loggingBuilder =>
-                {
-                    loggingBuilder.SetMinimumLevel(LogLevel.Information);
-                    loggingBuilder.AddDebug();
-                    loggingBuilder.AddProvider(new XunitLoggingProvider(output));
-                });
-            services.AddScoped<IStringContract, StringImplementor>();
-
-            IServiceProvider serviceProvider = services.BuildServiceProvider();
-            return serviceProvider;
-        }
-
-        private static Uri GetServerAddress()
-        {
-            string hostName = Dns.GetHostName();
-            IPAddress[] addresses = Dns.GetHostAddresses(hostName);
-            IPAddress ipv4Address = addresses.First(a => a.AddressFamily == AddressFamily.InterNetwork);
-
-            UriBuilder addressBuilder = new UriBuilder("tcp", ipv4Address.ToString(), 10678);
-            return addressBuilder.Uri;
-        }
-
-        private static PolyHost CreateHost(Uri serverAddress, IServiceProvider serviceProvider)
-        {
-            ITransport hostTransport = new TcpTransport(serverAddress);
-            IFormat hostFormat = new BinaryFormat();
-            PolyHost host = new PolyHost(hostTransport, hostFormat, serviceProvider);
-            return host;
-        }
-
-        private static PolyClient CreateClient(Uri serverAddress, ILoggerFactory loggerFactory)
-        {
-            ITransport clientTransport = new TcpTransport(serverAddress);
-            IFormat clientFormat = new BinaryFormat();
-            PolyClient client = new PolyClient(clientTransport, clientFormat, loggerFactory);
-            return client;
-        }
-
-        private async Task StartHost()
-        {
-            _host.Start();
-            await Task.Delay(100);
-        }
+        public Tests(ITestOutputHelper output) : base(output)
+        {}
 
         [Theory]
         [InlineData(1, 1)]
@@ -101,21 +27,21 @@ namespace PolyMessage.IntegrationTests.RequestResponse
             // arrange
             for (int i = 0; i < clientsCount; ++i)
             {
-                PolyClient client = CreateClient(_serverAddress, _serviceProvider.GetRequiredService<ILoggerFactory>());
-                _clients.Add(client);
+                PolyClient client = CreateClient(ServerAddress, ServiceProvider);
+                Clients.Add(client);
             }
 
             // act
-            _host.AddContract<IStringContract, StringImplementor>();
+            Host.AddContract<IStringContract, StringImplementor>();
             await StartHost();
 
             List<Task<double>> clientTasks = new List<Task<double>>();
-            foreach (PolyClient client in _clients)
+            foreach (PolyClient client in Clients)
             {
                 Task<double> clientTask = Task.Run(async () =>
                 {
                     TimeSpan duration = await MakeRequests(client, messagesCount);
-                    _logger.LogInformation("Making {0} requests from a client took: {1:0} ms.", messagesCount, duration.TotalMilliseconds);
+                    Logger.LogInformation("Making {0} requests from a client took: {1:0} ms.", messagesCount, duration.TotalMilliseconds);
                     return duration.TotalMilliseconds;
                 });
                 clientTasks.Add(clientTask);
