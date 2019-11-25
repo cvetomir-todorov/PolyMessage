203988e56cfd91a1ef449643bc0c728030d23758
cecotodorov <cvetomir.todorov@gmail.com>
2019-11-25 22:11:21 +0200

    Handle end of data stream in MessagePack formatter

--- src/PolyMessage.Formats.MessagePack/MessagePackFormatter.cs
+++ src/PolyMessage.Formats.MessagePack/MessagePackFormatter.cs
@@ -9,6 +9,7 @@ namespace PolyMessage.Formats.MessagePack
     {
         private readonly MessagePackFormat _format;
         private readonly PolyStream _channelStream;
+        private const string KnownErrorConnectionClosed = "Invalid MessagePack code was detected, code:-1";
 
         public MessagePackFormatter(MessagePackFormat format, PolyChannel channel)
         {
@@ -28,16 +29,31 @@ namespace PolyMessage.Formats.MessagePack
 
         public override Task Write(object obj, CancellationToken cancelToken)
         {
-            MessagePackSerializer.NonGeneric.Serialize(obj.GetType(), _channelStream, obj, MessagePackSerializer.DefaultResolver);
-            _channelStream.Flush();
-            return Task.CompletedTask;
+            try
+            {
+                MessagePackSerializer.NonGeneric.Serialize(obj.GetType(), _channelStream, obj, MessagePackSerializer.DefaultResolver);
+                _channelStream.Flush();
+                return Task.CompletedTask;
+            }
+            catch (InvalidOperationException exception) when (exception.Message.StartsWith(KnownErrorConnectionClosed))
+            {
+                throw new PolyFormatException(PolyFormatError.EndOfDataStream, _format);
+            }
         }
 
         public override Task<object> Read(Type objType, CancellationToken cancelToken)
         {
-            // FEAT: using readStrict is slow, but their API is limited
-            object obj = MessagePackSerializer.NonGeneric.Deserialize(objType, _channelStream, MessagePackSerializer.DefaultResolver, readStrict: true);
-            return Task.FromResult(obj);
+            try
+            {
+                // FEAT: using readStrict is slow, but their API is limited
+                object obj = MessagePackSerializer.NonGeneric.Deserialize(objType, _channelStream, MessagePackSerializer.DefaultResolver,
+                    readStrict: true);
+                return Task.FromResult(obj);
+            }
+            catch (InvalidOperationException exception) when (exception.Message.StartsWith(KnownErrorConnectionClosed))
+            {
+                throw new PolyFormatException(PolyFormatError.EndOfDataStream, _format);
+            }
         }
     }
 }
