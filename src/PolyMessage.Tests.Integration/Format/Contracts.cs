1e6207ba91d3cc369616aaa9841fdabc90a61b48
cecotodorov <cvetomir.todorov@gmail.com>
2019-11-25 13:16:37 +0200

    Add basic formatters tests about types

---
+++ src/PolyMessage.Tests.Integration/Format/Contracts.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace PolyMessage.Tests.Integration.Format
+{
+    [PolyContract]
+    public interface IContract
+    {
+        [PolyRequestResponse]
+        Task<SimpleTypesResponse> SimpleTypes(SimpleTypesRequest request);
+
+        [PolyRequestResponse]
+        Task<ValueTypesResponse> ValueTypes(ValueTypesRequest request);
+
+        [PolyRequestResponse]
+        Task<ClassesResponse> Classes(ClassesRequest request);
+    }
+
+    public class Implementor : IContract
+    {
+        public SimpleTypesRequest LastSimpleTypesRequest { get; private set; }
+        public ValueTypesRequest LastValueTypesRequest { get; private set; }
+        public ClassesRequest LastClassesRequest { get; private set; }
+
+        public Task<SimpleTypesResponse> SimpleTypes(SimpleTypesRequest request)
+        {
+            LastSimpleTypesRequest = request;
+            return Task.FromResult(new SimpleTypesResponse());
+        }
+
+        public Task<ValueTypesResponse> ValueTypes(ValueTypesRequest request)
+        {
+            LastValueTypesRequest = request;
+            return Task.FromResult(new ValueTypesResponse());
+        }
+
+        public Task<ClassesResponse> Classes(ClassesRequest request)
+        {
+            LastClassesRequest = request;
+            return Task.FromResult(new ClassesResponse());
+        }
+    }
+
+    [PolyMessage]
+    [Serializable]
+    [DataContract]
+    public sealed class SimpleTypesRequest
+    {
+        // signed
+        [DataMember(Order = 1)] public sbyte SByte { get; set; }
+        [DataMember(Order = 2)] public Int16 Int16 { get; set; }
+        [DataMember(Order = 3)] public Int32 Int32 { get; set; }
+        [DataMember(Order = 4)] public Int64 Int64 { get; set; }
+        // floating point
+        [DataMember(Order = 5)] public Single Single { get; set; }
+        [DataMember(Order = 6)] public Double Double { get; set; }
+        [DataMember(Order = 7)] public Decimal Decimal { get; set; }
+        // unsigned
+        [DataMember(Order = 8)] public byte Byte { get; set; }
+        [DataMember(Order = 9)] public UInt16 UInt16 { get; set; }
+        [DataMember(Order = 10)] public UInt32 UInt32 { get; set; }
+        [DataMember(Order = 11)] public UInt64 UInt64 { get; set; }
+        // others
+        [DataMember(Order = 12)] public bool Bool { get; set; }
+        [DataMember(Order = 13)] public char Char { get; set; }
+    }
+
+    [PolyMessage]
+    [Serializable]
+    [DataContract]
+    public sealed class SimpleTypesResponse
+    {}
+
+    [Serializable]
+    [DataContract]
+    public enum CustomEnum
+    {
+        Default, Normal, Extreme = int.MaxValue
+    }
+
+    [Serializable]
+    [DataContract]
+    public struct CustomStruct
+    {
+        [DataMember(Order = 1)] public int ID { get; set; }
+        [DataMember(Order = 2)] public string Data { get; set; }
+    }
+
+    [PolyMessage]
+    [Serializable]
+    [DataContract]
+    public sealed class ValueTypesRequest
+    {
+        [DataMember(Order = 1)] public CustomEnum Enum { get; set; }
+        [DataMember(Order = 2)] public CustomStruct Struct { get; set; }
+    }
+
+    [PolyMessage]
+    [Serializable]
+    [DataContract]
+    public sealed class ValueTypesResponse
+    {}
+
+    [Serializable]
+    [DataContract]
+    public sealed class CustomClassRoot
+    {
+        [DataMember(Order = 1)] public CustomClassMiddle Middle1 { get; set; }
+        [DataMember(Order = 2)] public CustomClassMiddle Middle2 { get; set; }
+    }
+
+    [Serializable]
+    [DataContract]
+    public sealed class CustomClassMiddle
+    {
+        [DataMember(Order = 1)] public CustomClassLeaf Leaf { get; set; }
+    }
+
+    [Serializable]
+    [DataContract]
+    public sealed class CustomClassLeaf
+    {
+        [DataMember(Order = 1)] public int ID { get; set; }
+    }
+
+    [PolyMessage]
+    [Serializable]
+    [DataContract]
+    public sealed class ClassesRequest
+    {
+        [DataMember(Order = 1)] public CustomClassRoot Root { get; set; }
+    }
+
+    [PolyMessage]
+    [Serializable]
+    [DataContract]
+    public sealed class ClassesResponse
+    {}
+}
---
+++ src/PolyMessage.Tests.Integration/Format/TypeTests.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PolyMessage.Tests.Integration.Format
+{
+    public abstract class TypeTests : IntegrationFixture
+    {
+        private readonly Implementor _implementorInstance;
+
+        protected TypeTests(ITestOutputHelper output) : base(output, services =>
+        {
+            services.AddSingleton<IContract, Implementor>();
+        })
+        {
+            _implementorInstance = (Implementor) ServiceProvider.GetRequiredService<IContract>();
+            Client = CreateClient();
+
+            Client.AddContract<IContract>();
+            Host.AddContract<IContract>();
+        }
+
+        [Theory]
+        [InlineData(
+            SByte.MinValue, Int16.MinValue, Int32.MinValue, Int64.MinValue,
+            Single.MinValue, Double.MinValue, -123456789123456789,
+            Byte.MinValue, UInt16.MinValue, UInt32.MinValue, UInt64.MinValue,
+            false, 'a')]
+        [InlineData(
+            SByte.MaxValue, Int16.MaxValue, Int32.MaxValue, Int64.MaxValue,
+            Single.MaxValue, Double.MaxValue, 123456789123456789,
+            Byte.MaxValue, UInt16.MaxValue, UInt32.MaxValue, UInt64.MaxValue,
+            true, 'Z')]
+        public async Task SimpleTypes(
+            // signed
+            SByte @sbyte, Int16 int16, Int32 int32, Int64 int64,
+            // floating point
+            Single single, Double @double, Decimal @decimal,
+            // unsigned
+            Byte @byte, UInt16 uint16, UInt32 uint32, UInt64 uint64,
+            // others
+            bool @bool, char @char)
+        {
+            // arrange
+            SimpleTypesRequest request = new SimpleTypesRequest();
+            // signed
+            request.SByte = @sbyte;
+            request.Int16 = int16;
+            request.Int32 = int32;
+            request.Int64 = int64;
+            // floating point
+            request.Single = single;
+            request.Double = @double;
+            request.Decimal = @decimal;
+            // unsigned
+            request.Byte = @byte;
+            request.UInt16 = uint16;
+            request.UInt32 = uint32;
+            request.UInt64 = uint64;
+            // others
+            request.Bool = @bool;
+            request.Char = @char;
+
+            // act
+            await StartHost();
+            Client.Connect();
+            await Client.Get<IContract>().SimpleTypes(request);
+
+            // assert
+            _implementorInstance.LastSimpleTypesRequest.Should().BeEquivalentTo(request);
+        }
+
+        [Theory]
+        [InlineData(CustomEnum.Default, 0, "")]
+        [InlineData(CustomEnum.Normal, 12345, "normal")]
+        [InlineData(CustomEnum.Extreme, int.MaxValue, "texttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttexttext")]
+        public async Task ValueTypes(CustomEnum @enum, int structID, string structData)
+        {
+            // arrange
+            ValueTypesRequest request = new ValueTypesRequest();
+            request.Enum = @enum;
+            request.Struct = new CustomStruct {ID = structID, Data = structData};
+
+            // act
+            await StartHost();
+            Client.Connect();
+            await Client.Get<IContract>().ValueTypes(request);
+
+            // assert
+            _implementorInstance.LastValueTypesRequest.Should().BeEquivalentTo(request);
+        }
+
+        [Fact]
+        public async Task Classes()
+        {
+            // arrange
+            ClassesRequest request = new ClassesRequest();
+            request.Root = new CustomClassRoot();
+            request.Root.Middle1 = new CustomClassMiddle();
+            request.Root.Middle2 = new CustomClassMiddle();
+            request.Root.Middle1.Leaf = new CustomClassLeaf {ID = 456};
+            request.Root.Middle2.Leaf = new CustomClassLeaf {ID = 123};
+
+            // act
+            await StartHost();
+            Client.Connect();
+            await Client.Get<IContract>().Classes(request);
+
+            // assert
+            _implementorInstance.LastClassesRequest.Should().BeEquivalentTo(request);
+        }
+    }
+}
