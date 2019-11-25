1d313d79f2e8c18e124e75728c018fdb957ddb64
cecotodorov <cvetomir.todorov@gmail.com>
2019-11-25 21:35:02 +0200

    Leave transport-oriented tests without combinations of formats

--- src/PolyMessage.Tests.Combinations/Connection/AddressesTests.cs
+++ src/PolyMessage.Tests.Combinations/Connection/AddressesTests.cs
@@ -1,25 +1,16 @@
 ﻿using PolyMessage.Formats.DotNetBinary;
-using PolyMessage.Formats.MessagePack;
 using Xunit.Abstractions;
 
 namespace PolyMessage.Tests.Combinations.Connection
 {
     namespace AddressesTests
     {
-        public class Tcp_DotNetBinary : Integration.Connection.AddressesTests
+        public class Tcp : Integration.Connection.AddressesTests
         {
-            public Tcp_DotNetBinary(ITestOutputHelper output) : base(output)
+            public Tcp(ITestOutputHelper output) : base(output)
             {}
             protected override PolyFormat CreateHostFormat() => new DotNetBinaryFormat();
             protected override PolyFormat CreateClientFormat() => new DotNetBinaryFormat();
-        }
-
-        public class Tcp_MessagePack : Integration.Connection.AddressesTests
-        {
-            public Tcp_MessagePack(ITestOutputHelper output) : base(output)
-            {}
-            protected override PolyFormat CreateHostFormat() => new MessagePackFormat();
-            protected override PolyFormat CreateClientFormat() => new MessagePackFormat();
         }
     }
 }
--- src/PolyMessage.Tests.Combinations/TimeoutTests.cs
+++ src/PolyMessage.Tests.Combinations/TimeoutTests.cs
@@ -1,25 +1,16 @@
 ﻿using PolyMessage.Formats.DotNetBinary;
-using PolyMessage.Formats.MessagePack;
 using Xunit.Abstractions;
 
 namespace PolyMessage.Tests.Combinations
 {
     namespace TimeoutTests
     {
-        public class Tcp_DotNetBinary : Integration.Tcp.TimeoutTests
+        public class Tcp : Integration.Tcp.TimeoutTests
         {
-            public Tcp_DotNetBinary(ITestOutputHelper output) : base(output)
+            public Tcp(ITestOutputHelper output) : base(output)
             {}
             protected override PolyFormat CreateHostFormat() => new DotNetBinaryFormat();
             protected override PolyFormat CreateClientFormat() => new DotNetBinaryFormat();
-        }
-
-        public class Tcp_MessagePack : Integration.Tcp.TimeoutTests
-        {
-            public Tcp_MessagePack(ITestOutputHelper output) : base(output)
-            {}
-            protected override PolyFormat CreateHostFormat() => new MessagePackFormat();
-            protected override PolyFormat CreateClientFormat() => new MessagePackFormat();
         }
     }
 }
