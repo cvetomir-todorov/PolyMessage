f5abc7c9c9a5a0aed5c1281be205a444fb8eae47
cecotodorov <cvetomir.todorov@gmail.com>
2019-11-29 17:13:01 +0200

    Split the format and transport APIs into separate files

---
+++ src/PolyMessage/FormatApi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PolyMessage
+{
+    public abstract class PolyFormat
+    {
+        public abstract string DisplayName { get; }
+
+        public virtual void RegisterMessageTypes(IEnumerable<Type> messageTypes) { }
+
+        public abstract PolyFormatter CreateFormatter(PolyChannel channel);
+
+        public override string ToString()
+        {
+            return DisplayName;
+        }
+    }
+
+    public abstract class PolyFormatter : IDisposable
+    {
+        public void Dispose()
+        {
+            DoDispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void DoDispose(bool isDisposing) { }
+
+        public abstract PolyFormat Format { get; }
+
+        public abstract Task Write(object obj, CancellationToken cancelToken);
+
+        public abstract Task<object> Read(Type objType, CancellationToken cancelToken);
+
+        public override string ToString()
+        {
+            return $"Formatter for {Format.DisplayName}";
+        }
+    }
+}
--- src/PolyMessage/TransportApi.cs
+++ src/PolyMessage/TransportApi.cs
@@ -1,36 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace PolyMessage
 {
-    public abstract class PolyFormat
-    {
-        public abstract string DisplayName { get; }
-
-        public virtual void RegisterMessageTypes(IEnumerable<Type> messageTypes) {}
-
-        public abstract PolyFormatter CreateFormatter(PolyChannel channel);
-    }
-
-    public abstract class PolyFormatter : IDisposable
-    {
-        public void Dispose()
-        {
-            DoDispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        protected virtual void DoDispose(bool isDisposing) {}
-
-        public abstract PolyFormat Format { get; }
-
-        public abstract Task Write(object obj, CancellationToken cancelToken);
-
-        public abstract Task<object> Read(Type objType, CancellationToken cancelToken);
-    }
-
     public abstract class PolyTransport
     {
         public abstract string DisplayName { get; }
@@ -40,6 +13,11 @@ namespace PolyMessage
         public abstract PolyListener CreateListener();
 
         public abstract PolyChannel CreateClient();
+
+        public override string ToString()
+        {
+            return DisplayName;
+        }
     }
 
     public abstract class PolyListener : IDisposable
